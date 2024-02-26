76a2767f3e55c0256c78a83048635d8ff4a464a9
Erkenoss <xavier.bertin6@gmail.com>
2024-02-26 20:35:21 +0100

    Remove invincibility in HealthPlayer script

--- Assets/StarterAssets/ThirdPersonController/Scripts/HealthBar/Health.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/HealthBar/Health.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
 
-    public CharactersAttacks _charactersAttacks;
     public int curHealth = 0;
     public int maxHealth = 100;
     public HealthBar healthBar;
     void Start()
     {
-        _charactersAttacks = GetComponent<CharactersAttacks>();
         curHealth = maxHealth;
     }
     public void DamagePlayer(int damage)
     {
-        if (!_charactersAttacks.invincible)
-        {
-            curHealth -= damage;
-            healthBar.SetHealth(curHealth);
-        }
+        curHealth -= damage;
+        healthBar.SetHealth(curHealth);
     }
 }
