3b0a733d12e6a13cdca49528d62ea5cda0b99f5c
Erkenoss <xavier.bertin6@gmail.com>
2024-03-12 23:11:50 +0100

    Add particle on rotor

--- Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Objects/RotorInteractable.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Objects/RotorInteractable.cs
@@ -7,21 +7,29 @@ public class RotorInteractable : MonoBehaviour, IInteractable
     public Vector3 rotation;
     public float speed;
     private Quaternion targetRotation;
+    private ParticleSystem spark;
 
     private void Start()
     {
         targetRotation = transform.rotation;
+        spark = GetComponent<ParticleSystem>();
     }
 
     private void ToggleMotor()
     {
         targetRotation *= Quaternion.Euler(rotation);
+        spark.Play();
     }
 
     private void Update()
     {
+        spark.Stop();
         //Start to go in direction of the target (interpolation) given in the editor on the x axes
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, speed * Time.deltaTime);
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+        {
+            targetRotation = transform.rotation;
+        }
     }
 
     public void Interact(Transform interactorTransform)
