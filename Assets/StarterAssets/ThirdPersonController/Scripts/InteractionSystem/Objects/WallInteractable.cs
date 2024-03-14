a9fd7032c106802108f2b8b94425e462de5b4d94
Erkenoss <xavier.bertin6@gmail.com>
2024-03-14 02:00:22 +0100

    Set Ragdoll part on main character

--- Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Objects/WallInteractable.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Objects/WallInteractable.cs
@@ -1,62 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Cinemachine;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Cinemachine;
+// using TMPro;
 
-public class WallInteractable : MonoBehaviour, IInteractable
-{
-    public CinemachineVirtualCamera mainCamera;
-    public CinemachineVirtualCamera windowCamera;
-    public TextMeshPro textMain;
-    public TextMeshPro textWindow;
-    public bool windowMode;
+// public class WallInteractable : MonoBehaviour, IInteractable
+// {
+//     public CinemachineVirtualCamera mainCamera;
+//     public CinemachineVirtualCamera windowCamera;
+//     public TextMeshPro textMain;
+//     public TextMeshPro textWindow;
+//     public bool windowMode;
 
 
-    private void Start()
-    {
-        textMain.gameObject.SetActive(false);
-        textWindow.gameObject.SetActive(false);
+//     private void Start()
+//     {
+//         // textMain.gameObject.SetActive(false);
+//         textWindow.gameObject.SetActive(false);
 
-        mainCamera.enabled = !windowMode;
-        windowCamera.enabled = windowMode;
-    }
+//         mainCamera.enabled = !windowMode;
+//         windowCamera.enabled = windowMode;
+//     }
 
-    private IEnumerator DeleteText(TextMeshPro text)
-    {
-        yield return new WaitForSeconds(5);
-        Destroy(text.gameObject);
-    }
-    private void ToggleWindow()
-    {
-        windowMode = !windowMode;
-        if (windowMode)
-        {
-            textMain.gameObject.SetActive(windowMode);
-            StartCoroutine(DeleteText(textMain));
-        }
-        else
-        {
-            textWindow.gameObject.SetActive(!windowMode);
-            StartCoroutine(DeleteText(textWindow));
-        }
+//     private IEnumerator DeleteText(TextMeshPro text)
+//     {
+//         yield return new WaitForSeconds(5);
+//         Destroy(text.gameObject);
+//     }
+//     private void ToggleWindow()
+//     {
+//         windowMode = !windowMode;
+//         if (windowMode)
+//         {
+//             textMain.gameObject.SetActive(windowMode);
+//             StartCoroutine(DeleteText(textMain));
+//         }
+//         else
+//         {
+//             textWindow.gameObject.SetActive(!windowMode);
+//             StartCoroutine(DeleteText(textWindow));
+//         }
 
-        mainCamera.enabled = !windowMode;
-        windowCamera.enabled = windowMode;
-    }
+//         mainCamera.enabled = !windowMode;
+//         windowCamera.enabled = windowMode;
+//     }
 
-    public void Interact(Transform interactorTransform)
-    {
-        ToggleWindow();
-    }
+//     public void Interact(Transform interactorTransform)
+//     {
+//         ToggleWindow();
+//     }
 
-    public string GetInteractText()
-    {
-        return "Check Outside";
-    }
+//     public string GetInteractText()
+//     {
+//         return "Check Outside";
+//     }
 
-    public Transform GetTransform()
-    {
-        return transform;
-    }
-}
+//     public Transform GetTransform()
+//     {
+//         return transform;
+//     }
+// }
